8ac4504063a258611a6a3647e4797c989beff619
goandreus <andreshtml5@gmail.com>
2020-06-16 02:06:00 -0500

    first variable asinged

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var dog, cat string = ":dog", ":cat"
+
+	//var dog, cat = ":dog", ":cat"
+
+	dog, cat := ":dog", ":cat"
+	//cat = "cat"
+
+	cat, face := "cat", ":face"
+
+	fmt.Println(dog, cat, face)
+}
