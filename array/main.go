8174ac06c03170450c8a97878df9f0027eb5cd4d
goandreus <andreshtml5@gmail.com>
2020-06-17 13:31:56 -0500

    arrays

---
+++ array/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var food [3]string
+	// food[0] = "🌭"
+	// food[1] = "🍕"
+	// food[2] = "🍕"
+
+	//array literal
+
+	food := [3]string{"🌭", "🍕", "🍕"}
+
+	fmt.Println(food)
+}
